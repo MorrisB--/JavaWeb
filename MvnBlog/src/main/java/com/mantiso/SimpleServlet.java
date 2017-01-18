767f51e6de36eab28e533ca4de5afd908674a5d9
MorrisB-- <morris.ballenger@yahoo.com>
2017-01-17 21:50:48 -0600

    Using servlet context

--- MvnBlog/src/main/java/com/mantiso/SimpleServlet.java
+++ MvnBlog/src/main/java/com/mantiso/SimpleServlet.java
@@ -16,7 +16,7 @@ public class SimpleServlet extends HttpServlet {
 
 	@Override
 	public void init() throws ServletException {
-		appName = getInitParameter("ProductName");
+		appName = getServletContext().getInitParameter("ProductName");
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
